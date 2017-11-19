221add736aa84b5b72b5d171756d7b529059f0c6
hziling <leonhanxl@gmail.com>
2017-11-19 14:10:36 +0800

    add logistic regression

---
+++ lr.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from common import *
+
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
