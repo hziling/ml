4a1a6409fcf4a9ab2e3ecbc55e31b63d5e153bc2
hziling <leonhanxl@gmail.com>
2018-01-07 15:51:42 +0800

    add pca

---
+++ pca.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from common import *
+
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = LogisticRegression()
+lr.fit(X_train_pca, y_train)
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
